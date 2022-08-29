dabeea9082bab0340b1eb4129672617688b89c21
Suarez-DDC <marc.suarez@desidedatum.com>
2022-08-29 14:09:08 +0200

    se añade la template de environments en una carpeta environments

---
+++ src/environments/environment-template.ts
@@ -0,0 +1,19 @@
+/* export const environment = {
+  production: true,
+  epsgCode: '',
+  aragonBoundingBox: [],
+  wmsVersion: '',
+  wmsLayers:'',
+  urlWMSServer: '',
+  urlSitaWMS: '',
+  urlVisor2D: '',
+  urlTypedSearchService: '',
+  urlSpatialSearchService: '',
+  typedSearchCP: '',
+  typedSearchDIRECCION: '',
+  typedSearchLOCALIDAD: '',
+  typenameCP: '',
+  typenameDIRECCION: '',
+  typenameLOCALIDAD: '',
+};
+ */
