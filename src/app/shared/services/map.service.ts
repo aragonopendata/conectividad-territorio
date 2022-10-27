f594bdeede579b194e51ca883fbfcf46dee6ba6a
Daniel de Mingo <danieldemingo@protonmail.com>
2022-10-27 12:21:37 +0200

    Adición de zoom más alejado para la capa viviendas en zonas negras

--- src/app/shared/services/map.service.ts
+++ src/app/shared/services/map.service.ts
@@ -172,7 +172,7 @@ export class MapService {
         result = this.buildCircleStyle(this.getHousingBlackZonesStyle((feature as any).values_.tipo_zona))
       break;
       default:
-        break;
+      break;
      }
      return result;
    }
@@ -282,7 +282,21 @@ export class MapService {
    getHousingBlackZonesStyle(affectedValue): FeatureColorStyle {    
 
     let result: FeatureColorStyle = {fillColor: "green", strokeColor: "#FFFFFF"};
-  
+/*
+      if(affectedValue <= 20){
+        result =  {fillColor: "#EF8480", strokeColor: "#FFFFFF"};
+      }else if (affectedValue > 20 && affectedValue <= 40){
+        result =  {fillColor: "#E94F49", strokeColor: "#FFFFFF"};
+      }else if (affectedValue > 40 && affectedValue <= 60){
+        result =  {fillColor: "#DA211B", strokeColor: "#FFFFFF"};
+      }else if (affectedValue > 60 && affectedValue <= 80){
+        result =  {fillColor: "#A3191A", strokeColor: "#FFFFFF"};
+      }else if (affectedValue > 80 && affectedValue <= 100){        
+        result =  {fillColor: "#6D110D", strokeColor: "#FFFFFF"};
+    }
+*/
+    return result;
+   }
 
   /**
    * 
@@ -320,9 +334,10 @@ export class MapService {
     let featuresNucleosUrbanos = featuresStyled.filter(item => item.id_.includes('nucleos_zbg_') && item.values_.geometry.flatCoordinates !== undefined);
     let featuresUnidadesInmobiliares = featuresStyled.filter(item => item.id_.includes('ui_zbg_') && item.values_.geometry.flatCoordinates !== undefined);
     let featuresIsdt = featuresStyled.filter(item => item.id_.includes('isdt_municipio') && item.values_.geometry.flatCoordinates !== undefined); 
+    let featuresViviendasZonasNegras = featuresStyled.filter(item => item.id_.includes('viviendas_zn_') && item.values_.geometry.flatCoordinates !== undefined); 
 
     let otherFeatures = featuresStyled.filter(item => !item.id_.includes('ui_zbg_') 
-        && !item.id_.includes('nucleos_zbg_') && !item.id_.includes('isdt_municipio'));
+        && !item.id_.includes('nucleos_zbg_') && !item.id_.includes('isdt_municipio') && !item.id_.includes('viviendas_zn_'));
 
     let vectorLayer = new VectorLayer({
       source: new VectorSource({
@@ -502,17 +517,79 @@ export class MapService {
 
           olMap.getView().fit(vectorLayer.getSource().getExtent());
 
+        }      
+    }
+
+    if(featuresViviendasZonasNegras != undefined && featuresViviendasZonasNegras.length > 0)
+    {
+        let coordinatesAndStyles = featuresViviendasZonasNegras.map(function(item){
+            return {coordinates: item.values_.geometry.flatCoordinates.slice(0, 2), style: item.style_}
+        });
+
+        let newFeatures : Feature[] = [];
+        for(let newFeature of coordinatesAndStyles)
+        {
+            let _f = new Feature({
+                geometry: new Point(newFeature.coordinates)
+            });
+
+            _f.setStyle(newFeature.style)
+            newFeatures.push(_f);
         }
 
-        
-    }
-    
-   // olMap.getView().fit(extent);
-    olMap.getView().setZoom(14.5);
-//    olMap.getView().setMaxZoom(18);
-    olMap.getView().setMinZoom(14.5);
+        let vectorLayerViviendasLejos = new VectorLayer({
+            source: new VectorSource({
+            format: geojsonFormat,
+            features: newFeatures,
+            }),
+            style: new Style({
+            image: new Circle({
+                fill: fill,
+                stroke: stroke,
+                radius: 5,
+            }),
+            fill: fill,
+            stroke: stroke,
+            }),
+            className: className,
+            minResolution: 120
+        });
 
+        let vectorLayerViviendasCerca = new VectorLayer({
+            source: new VectorSource({
+              format: geojsonFormat,
+              features: featuresViviendasZonasNegras,
+            }),
+            style: new Style({
+              image: new Circle({
+                fill: fill,
+                stroke: stroke,
+                radius: 5,
+              }),
+              fill: fill,
+              stroke: stroke,
+            }),
+            className: className,
+            maxResolution: 120
+          });
 
+        olMap.addLayer(vectorLayerViviendasCerca)
+        olMap.addLayer(vectorLayerViviendasLejos);
+        if (vectorLayerViviendasCerca != null){
+
+          olMap.getView().fit(vectorLayerViviendasCerca.getSource().getExtent());
+
+        }
+        // olMap.getView().fit(extent);
+        olMap.getView().setZoom(10.5);
+        // olMap.getView().setMaxZoom(18);
+        olMap.getView().setMinZoom(10.5);
+
+    } else {                   
+        // olMap.getView().fit(extent);
+        olMap.getView().setZoom(14.5);
+        // olMap.getView().setMaxZoom(18);
+        olMap.getView().setMinZoom(14.5);}
  
   }
 
