d14262715533ac925b2a6cbcecbd3bfeb81d9890
DYST0P1A <57276464+DYST0P1A@users.noreply.github.com>
2022-11-30 12:50:19 +0100

    Revert "Adición del color verde para la capa de viviendas en zonas negras"
    This reverts commit 29b892c3cf649df6bb62e3f540282cfcb3236124.

--- src/app/shared/services/map.service.ts
+++ src/app/shared/services/map.service.ts
@@ -168,9 +168,6 @@ export class MapService {
       case 'ui_zbg_2022_x_muni':
         result = this.buildCircleStyle(this.getBuildingUnitStyle((feature as any).values_.tipo_zona))
       break;
-      case 'viviendas_zn_2022_x_muni':
-        result = this.buildCircleStyle(this.getHousingBlackPlacesStyle((feature as any).values_.tipo_zona))
-      break;
       default:
         break;
      }
@@ -279,9 +276,6 @@ export class MapService {
     return result;
    }
 
-   getHousingBlackPlacesStyle(affectedValue): FeatureColorStyle {    
-
-    let result: FeatureColorStyle = {fillColor: "green", strokeColor: "#FFFFFF"};
   
 
   /**
