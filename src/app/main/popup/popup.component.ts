0765449c7db458f151172722dcf46b06e6f4fe43
DYST0P1A <57276464+DYST0P1A@users.noreply.github.com>
2022-11-30 12:50:26 +0100

    Revert "Adición del popup para viviendas en zonas negras con los correspondientes campos"
    This reverts commit ac358dc703ce1e2540dfa3b41d9b775f1498c0f9.

--- src/app/main/popup/popup.component.ts
+++ src/app/main/popup/popup.component.ts
@@ -195,15 +195,6 @@ export class PopupComponent implements OnInit, AfterViewInit, OnChanges {
 
         map.set('Factor escenario y patrimonio', {value: this.getFeatureValue(featureSelect, "f_escenario_patrimonio" ), format: ""} );
       break;
-      case "viviendas_zn_2022_x_muni" :
-        map.set(this.TITLE, {value: "Municipio: " + this.getFeatureValue(featureSelect, "municipio" )+ " (2022)", format: ""});       
-        
-        map.set('Código municipio ine', {value: this.getFeatureValue(featureSelect, "c_muni_ine" ), format: ""} );
-
-        map.set('Viviendas en zona negra', {value: this.getFeatureValue(featureSelect,"viviendas_zonanegra"), format: ""} );
-
-        map.set('Porcentaje de viviendas totales', {value: this.getFeatureValue(featureSelect,"porcentaje_viviendas_zonanegra"), format: "||value|| %"} );
-      break;
       default:
         break;
     }
